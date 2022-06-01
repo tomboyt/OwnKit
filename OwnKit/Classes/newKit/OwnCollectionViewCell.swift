8b8f213dc79ca4839a0780c0d3c691ba7322018b
tomboyt <tomboyt@163.com>
2022-06-01 15:19:23 +0800

    2change

---
+++ OwnKit/Classes/newKit/OwnCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  OwnCollectionViewCell.swift
+//  OwnKit_Example
+//
+//  Created by tangquan on 2022/6/1.
+//  Copyright © 2022 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class OwnCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
